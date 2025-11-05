9bbe869673ad3442bf6307d257cbcb28a18e692f
dima <dimasmirnov0207@gmail.com>
2025-11-05 17:49:38 +0300

    Создание формы администратора

--- pr08/Program.cs
+++ pr08/Program.cs
@@ -16,7 +16,7 @@ namespace pr08
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormManager());
+            Application.Run(new FormAdministrator());
         }
     }
 }
